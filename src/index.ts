ab1e1ca092b1a4b8fc0150408e540e1e2f8a7038
nouraizasad <nouraizasad@hotmail.com>
2024-04-26 02:46:49 +0500

    express app created

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import express from 'express';
+import { eventsController } from './modules/events/controller';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
